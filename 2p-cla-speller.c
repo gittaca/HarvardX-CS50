0c10ea9904f3f349dce7196c586138bb81144790
Gittaca <gittaca@nerdpol.ch>
2016-03-05 18:48:42 +0100

    succeed on 2nd try
    for loop needs its own counter initialisation

--- 2p-cla-speller.c
+++ 2p-cla-speller.c
@@ -8,11 +8,11 @@
 
 int main(int argc, char* argv[])
 {
-    for (i = 0; i < argc; i++)
+    for (int i = 0; i < argc; i++)
     {
         printf("CLA no. %i is \'%s\' and is spelled:", i + 1, argv[i]);
 
-        for (j = 0; j < strlen(argv[i]); j++)
+        for (int j = 0; j < strlen(argv[i]); j++)
         {
             printf(" %c", argv[i][j]);
         }
