499624aabd4e055653b45f54b771a6a726cf765e
Gittaca <gittaca@nerdpol.ch>
2016-03-05 18:56:47 +0100

    convert outer for- to do-while-loop, which...
    ...executes at least once, because condition is checked at the end

--- 2p-cla-speller.c
+++ 2p-cla-speller.c
@@ -8,14 +8,20 @@
 
 int main(int argc, char* argv[])
 {
-    for (int i = 0; i < argc; i++)
+    int i = 0; // in for-loop: initialisation
+
+    do // executes at least once, because condition is checked at the end
     {
         printf("CLA no. %i is \'%s\' and is spelled:", i + 1, argv[i]);
 
         for (int j = 0; j < strlen(argv[i]); j++)
+        //   init.      condition            update
         {
             printf(" %c", argv[i][j]);
         }
+
         printf(".\n");
+        i++; // in for-loop: update
     }
+    while (i < argc); // in for-loop: condition, that's checked
 }
