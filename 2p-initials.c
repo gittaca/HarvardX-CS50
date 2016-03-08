6c65d298fc9777df8eb28c3d9fcbbe324ae04fdf
Gittaca <gittaca@nerdpol.ch>
2016-03-08 21:54:51 +0100

    add comments

--- 2p-initials.c
+++ 2p-initials.c
@@ -9,18 +9,22 @@ int main(void)
     string full_name = GetString();
     int length = strlen(full_name);
 
+    // assume 1st initial to be constantly the 1st letter given
     PrintInitialCap(full_name[0]);
 
+    // walk along remaining string
     for (int i = 0; i < length ; i++)
     {
         if (full_name[i] == ' ')
         {
+            // assume letter after space to be an initial
             PrintInitialCap(full_name[i + 1]);
         }
     }
     printf("\n");
 }
 
+// capitalise initial as shown in week 2 lecture
 void PrintInitialCap(char initial)
 {
     if (initial >= 'a' && initial <= 'z')
