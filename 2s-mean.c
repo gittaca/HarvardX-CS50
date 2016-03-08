27c753bda8746ebf5f7a00f01729725090e3f85a
Gittaca <gittaca@nerdpol.ch>
2016-03-08 18:59:59 +0100

    move summing into input loop

--- 2s-mean.c
+++ 2s-mean.c
@@ -5,6 +5,7 @@ int main(void)
 {
     int n = 5;
     int numbers[n];
+    int sum = 0;
 
     printf("Please type in %i positive integers!\n", n);
 
@@ -19,15 +20,14 @@ int main(void)
         while (m < 1);
 
         numbers[i] = m;
+        sum += numbers[i];
     }
 
     printf("OK, thanks! The numbers are:\n");
-    int sum = 0;
 
     for (int i = 0; i < n; i++)
     {
         printf("%i\n", numbers[i]);
-        sum += numbers[i];
     }
 
     printf("And the mean is: %2f.\n", (float) sum / n);
