4d5e7ee4bec9b03f58b0d5bab30daf1a6470877d
Gittaca <gittaca@nerdpol.ch>
2016-03-08 18:45:28 +0100

    fail on 1st try
    9:29: error: more '%' conversions than data arguments [-Werror,-Wformat]
        printf("Please type in %i positive integers!\n, n");
                                            ~^

---
+++ 2s-mean.c
@@ -0,0 +1,35 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    int n = 5;
+    int numbers[5];
+
+    printf("Please type in %i positive integers!\n, n");
+
+    for (int i = 0; i < n; i++)
+    {
+        int m;
+
+        do
+        {
+            m = GetInt();
+        }
+        while (m < 1);
+
+        numbers[i] = m;
+        i++;
+    }
+
+    printf("OK, thanks! The numbers are:\n");
+    int sum = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        printf("%i\n", numbers[i]);
+        sum += numbers[i];
+    }
+
+    printf("And the mean is: %2f.\n", (float) sum / n);
+}
