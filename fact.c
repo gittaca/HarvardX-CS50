144d25f9a6447cb9ba7772b2c3e32448347c08fd
Gittaca <gittaca@nerdpol.ch>
2016-03-27 11:20:53 +0200

    clunky approach, because...
    ...of helper variables. However, these are useful as breakpoints & debugging steps.

--- fact.c
+++ fact.c
@@ -3,6 +3,11 @@
 
 int fact(int m) {
     // recurse, till smallest logical factor or result
+    int result = 1;
+    if (m <= 1)
+        return result;
+    else
+        return result = m * fact(m - 1);
 }
 
 int main(int argc, char* argv[]) {
@@ -15,4 +20,7 @@ int main(int argc, char* argv[]) {
     }
     else
         n = atoi(argv[1]);
+
+    int product = fact(n);
+    printf("The factorial (n!) of %i is %i.\n", n, product);
 }
