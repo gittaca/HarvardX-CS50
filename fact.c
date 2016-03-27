ee383ca34fc79f1199d7603cf13e24cfeea41769
Gittaca <gittaca@nerdpol.ch>
2016-03-27 11:24:49 +0200

    shorter, more elegant approach, but...
    ...less variables & less lines mean fewer opportunities for breakpoints & debugging insights. When in doubt, start clunky & polish later!

--- fact.c
+++ fact.c
@@ -3,11 +3,10 @@
 
 int fact(int m) {
     // recurse, till smallest logical factor or result
-    int result = 1;
     if (m <= 1)
-        return result;
+        return 1;
     else
-        return result = m * fact(m - 1);
+        return m * fact(m - 1);
 }
 
 int main(int argc, char* argv[]) {
@@ -21,6 +20,5 @@ int main(int argc, char* argv[]) {
     else
         n = atoi(argv[1]);
 
-    int product = fact(n);
-    printf("The factorial (n!) of %i is %i.\n", n, product);
+    printf("The factorial (n!) of %i is %i.\n", n, fact(n));
 }
